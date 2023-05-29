aaf27a8cac6743744d9c36eebf8d181c354f6062
koudai397 <koudai.jaden@gmail.com>
2023-05-29 09:17:09 +0900

    add: first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  # ログインユーザー取得のルーティング
+  namespace :auth do
+    resources :sessions, only: %i[index]
+  end
+
+end
